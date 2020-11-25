e2e14333a934060a82fb1d43e618c45b9392f94f
tianhhuan <q1436816322@gmail.com>
2020-11-25 23:06:02 +0800

    循环

---
+++ note/OneDay.md
@@ -0,0 +1,2 @@
+ 1. for in 数组使用 .iter()
+ 2. loop 中break 可以带回参
--- src/main.rs
+++ src/main.rs
@@ -23,4 +23,69 @@ fn main() {
     println!("常量 lll ：{}",lll);
 
 
+    ///函数
+    GetAdd10(11);
+
+
+    ///函数的语句和表达式
+
+    let a ={
+        let a1=3;
+        a1+11
+    };
+    fn fn1()->i32{
+        5
+    }
+    println!("表达式a:{}, 函数表达式{}",a,fn1());
+
+    println!("有返回值方法：{}",GetReturnInt(11)) ;
+
+
+
+    ///条件语句
+
+    let index = 3;
+    if index>3 {
+        println!("idnex大于3")
+    }else if index==3 {
+        println!("index等于3")
+    }else {
+        println!("index小于3")
+    }
+
+    //循环
+    // while true {
+    //     println!("循环")
+    // }
+
+    let mut  leng=10;
+    while leng>1 {
+        println!("leng:{}",leng);
+        leng-=1;
+    }
+
+    let array=[1,2,3,4,5];
+    for item in array.iter() {
+        println!("for in:{}",item)
+    }
+    for  item in 0..10 {
+        println!("for in:{}",item)
+    }
+
+    let mut  leng1=10;
+    loop {
+        leng1-=1;
+        println!(" loop leng1:{}",leng1);
+        if leng1<0 {
+            break;
+        }
+    }
+}
+///无返回值
+fn GetAdd10(x:i32){
+    println!("x {}",x+10)
+}
+///有返回值
+fn GetReturnInt(x:i32)-> i32{
+    return  x+1;
 }
